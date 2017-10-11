a141dc090e04d08c49cc64fbbef8ab83eb3757b7
Regan-Koopmans <regankoopmans@protonmail.com>
2017-10-11 09:05:03 +0200

    Hermes now retrieves CNF paramter from POST

--- hermes/README.md
+++ hermes/README.md
@@ -1,1 +1,15 @@
 # Hermes
+
+Hermes is a small service that exposes the SATServer to 
+the HTTP protocol, allowing solving to be controlled from a
+web browser.
+
+## How to Run
+
+To start the service on the defaul port 8080, type the 
+following commands
+
+```
+npm i
+npm start
+```
--- hermes/hermes.js
+++ hermes/hermes.js
@@ -1,18 +1,55 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const HOST = 'localhost';
+const PORT = 55555;
 
-app.use(bodyParser.json())
+const express = require('express');
+const bodyParser = require('body-parser');
+const net = require('net');
 
-app.post('/', function(req, res) {
+const app = express();
+var client = new net.Socket();
+
+app.use(express.json()); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.post('/', function (req, res) {
+
+    // Check for presence of cnf parameter.
+    
     let cnf = req.body.cnf;
-    res.send('Hello');
+    if (cnf === undefined) {
+        res.send('You are going to need to send me a CNF problem to pass to Zeus.');
+        console.log('Received malformed request.');
+        return;
+    }
+    console.log("Received the following job:");
+    console.log(cnf);
+    console.log("Attempting to send job to SATServer...");
+
+    // Talk to SATServer.
+
+    // construct request
+    // write to socket.
+
+    
+
+    // Respond to browser.
+    res.send("Your solution is:\n[some bit string]");
 });
 
-app.get('/', function(req, res) {
-    res.send('Please use POST to talk to me.')
-})
+app.get('/', function (req, res) {
+    res.send('Please use POST to talk to me.');
+});
 
-app.listen(8080, function() {
-    console.log('Endpoint is exposed on port 8080.')
+app.listen(8080, function () {
+    console.log('Endpoint is exposed on port 8080.');
+});
+
+client.connect(PORT, HOST, function () {
+    console.log('Connected to SATServer at (' + HOST + ':' + PORT + ")");
+
+});
+
+client.on('close', function() {
+    console.log('Connection to SATServer closed.');
 });
