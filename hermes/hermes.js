d6015979753c38cb72470a87cbb6413c1997dbf6
Regan-Koopmans <regankoopmans@protonmail.com>
2017-10-11 08:12:43 +0200

    Added bodyParser

--- hermes/hermes.js
+++ hermes/hermes.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.json())
+
 app.post('/', function(req, res) {
-    res.send('Hello')
+    let cnf = req.body.cnf;
+    res.send('Hello');
 });
 
 app.get('/', function(req, res) {
-    res.send('Please use post to talk to me.')
+    res.send('Please use POST to talk to me.')
 })
 
 app.listen(8080, function() {
